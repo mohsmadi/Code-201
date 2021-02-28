9fe027455214ca68df04dbfababa0002412dacb9
mohsmadi <msmadi_7@yahoo.com>
2021-02-28 23:31:10 +0200

    add lab01b

---
+++ lab-01b/js/lap2.js
@@ -0,0 +1,12 @@
+let accountName='Mohammad';
+let accountNo=2040;
+const passWord=5151;
+let branchCode=41;
+console.log("before changes: ",accountName,accountNo,passWord,branchCode);
+accountNo;5566
+console.log("after changes: ",accountNo,passWord);
+
+alert('Your Welcom');
+confirm('Do you satisfy about our services?');
+
+let userInput=prompt('enter your branch code: ');
